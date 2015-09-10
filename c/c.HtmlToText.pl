b6dce122bea11be05d5eb591a225648d978092b1
Mariscal <marisscal@users.noreply.github.com>
2015-09-10 06:31:47 -0600

    Update c.HtmlToText.pl

--- c/c.HtmlToText.pl
+++ c/c.HtmlToText.pl
@@ -1,3 +1,8 @@
 my $tree = HTML::TreeBuilder->new->parse_content($body);
 my $formatter = HTML::FormatText->new(leftmargin => 0, rightmargin => 50);
 $body = $formatter->format($tree);
+
+# $body = encode('utf8',decode('iso-8859-1',$body));
+# $body = encode('iso-8859-1',decode('utf8',$body));
+
+print "Mensaje: " . $body."\n";
